474a17a0b159782bf276efeeb54a06c21f595a80
Decim123 <aleksandrtuman@gmail.com>
2023-09-17 12:01:29 +0300

    quick sort added

--- task-3-Sort.py
+++ task-3-Sort.py
@@ -1,15 +1,36 @@
 import random
 import math
 import json
+
 strtrng = 0
 endrng = 100
 array = []
+
 for i in range(strtrng, endrng):
     array.append(random.randint(1, 99))
 print('начальный массив -', array)
-array_p = array
-for i in range(endrng-1):
-    for j in range(endrng-i-1):
-        if array_p[j] > array_p[j+1]:
-            array_p[j], array_p[j+1] = array_p[j+1], array_p[j]
-print('результат пузырьковой сортировки -', array_p)
+
+array_bubble = array
+
+def bubblesort(x):
+    for i in range(endrng-1):
+        for j in range(endrng-i-1):
+            if x[j] > x[j+1]:
+                x[j], x[j+1] = x[j+1], x[j]            
+    return x
+
+print('результат пузырьковой сортировки -', bubblesort(array_bubble))
+
+array_quick = array
+
+def quicksort(x):
+   if len(x) <= 1:
+       return x
+   else:
+       q = random.choice(x)
+   l_x = [n for n in x if n < q]
+   e_x = [q] * x.count(q)
+   b_x = [n for n in x if n > q]
+   return quicksort(l_x) + e_x + quicksort(b_x)
+
+print('результат быстрой сортировки', quicksort(array_quick))
