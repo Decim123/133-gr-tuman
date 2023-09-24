e9ae78c18e3172e94c3f18d09697c7983fbb9cb3
Decim123 <aleksandrtuman@gmail.com>
2023-09-24 15:06:55 +0300

    same sym counter was added

--- task-4-Words.py
+++ task-4-Words.py
@@ -1,6 +1,8 @@
+from collections import Counter
+
 sentence = str(input('Введите предложение '))
 
-def counter(x):
+def words_counter(x):
     words = x.split(' ')
     count_words = len(words)
     return count_words
@@ -9,5 +11,17 @@ def replacer(x):
     rep = x.replace(input('заменяемый символ '), '😎')
     return rep
 
-print(counter(sentence))
+def split(x):
+    x = [char for char in x]
+    return x
+
+def same_symbols_couter(x):
+    x = split(x)
+    count = Counter(x)
+    return count
+
+print(words_counter(sentence))
 print(replacer(sentence))
+print(split(sentence))
+print(sentence)
+print(same_symbols_couter(sentence))
